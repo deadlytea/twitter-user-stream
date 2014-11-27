aa15c053963eaeccb6cf72bc8f747659c104a316
Kristian Spriggs <kristianspriggs@icloud.com>
2014-11-27 13:29:36 -0500

    added ability to use environment variables for twitter credentials, for some reason this causes go fmt to be run, hence the large commit

--- ustream.go
+++ ustream.go
@@ -1,189 +1,196 @@
 package ustream
 
 import (
-  "log"
-  "net/http"
-  "bufio"
-  "io/ioutil"
-  "bytes"
-  "strings"
-  "github.com/kurrik/oauth1a"
-  "encoding/json"
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"github.com/kurrik/oauth1a"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 type Tweet struct {
-  Id_str string
-  Text string
-  User *User
-  In_reply_to_status_id_str string
+	Id_str                    string
+	Text                      string
+	User                      *User
+	In_reply_to_status_id_str string
 }
 
 type User struct {
-  Screen_name string
-  Name string
+	Screen_name string
+	Name        string
 }
 
 type UStreamClient struct {
-  service *oauth1a.Service
-  user *oauth1a.UserConfig
-  httpClient *http.Client
-  stream chan *Tweet
+	service    *oauth1a.Service
+	user       *oauth1a.UserConfig
+	httpClient *http.Client
+	stream     chan *Tweet
 }
 
 func NewUStreamClient() *UStreamClient {
 
-  creds, err := ReadCredentials()
-  if err != nil {
-    log.Printf("Credential read error, could not create UStreamClient")
-    return nil
-  }
+	creds, err := ReadCredentials()
+	if err != nil {
+		log.Printf("Credential read error, could not create UStreamClient")
+		return nil
+	}
 
-  u := new(UStreamClient)
+	u := new(UStreamClient)
 
-  u.service = &oauth1a.Service{
-    RequestURL:   "https://api.twitter.com/oauth/request_token",
-    AuthorizeURL: "https://api.twitter.com/oauth/request_token",
-    AccessURL:    "https://api.twitter.com/oauth/request_token",
-    ClientConfig: &oauth1a.ClientConfig{
-        ConsumerKey:    creds["oauth_consumer_key"],
-        ConsumerSecret: creds["oauth_consumer_secret"],
-        CallbackURL:    "http://www.deadlytea.com", // unused
-    },
-    Signer: new(oauth1a.HmacSha1Signer),
-  }
+	u.service = &oauth1a.Service{
+		RequestURL:   "https://api.twitter.com/oauth/request_token",
+		AuthorizeURL: "https://api.twitter.com/oauth/request_token",
+		AccessURL:    "https://api.twitter.com/oauth/request_token",
+		ClientConfig: &oauth1a.ClientConfig{
+			ConsumerKey:    creds["oauth_consumer_key"],
+			ConsumerSecret: creds["oauth_consumer_secret"],
+			CallbackURL:    "http://www.deadlytea.com", // unused
+		},
+		Signer: new(oauth1a.HmacSha1Signer),
+	}
 
-  u.httpClient = &http.Client{
-    Transport: &http.Transport{},
-  }
+	u.httpClient = &http.Client{
+		Transport: &http.Transport{},
+	}
 
-  u.user = oauth1a.NewAuthorizedConfig(creds["oauth_token"], creds["oauth_token_secret"])
+	u.user = oauth1a.NewAuthorizedConfig(creds["oauth_token"], creds["oauth_token_secret"])
 
-  u.stream = make(chan *Tweet, 10)
+	u.stream = make(chan *Tweet, 10)
 
-  return u
+	return u
 }
 
 func ReadCredentials() (map[string]string, error) {
-  creds, err := ioutil.ReadFile("CREDENTIALS")
-  if err != nil {
-    return nil, err
-  }
+	c := make(map[string]string)
+	var creds_arr []string
+	if os.Getenv("OAUTH_CONSUMER_KEY") != "" {
+		creds_arr[0] = os.Getenv("OAUTH_CONSUMER_KEY")
+		creds_arr[1] = os.Getenv("OAUTH_CONSUMER_SECRET")
+		creds_arr[2] = os.Getenv("OAUTH_TOKEN")
+		creds_arr[3] = os.Getenv("OAUTH_TOKEN_SECRET")
+	} else {
+		creds, err := ioutil.ReadFile("CREDENTIALS")
+		if err != nil {
+			return nil, err
+		}
+		creds_arr = strings.Split(string(creds), "\n")
+	}
 
-  lines := strings.Split(string(creds), "\n")
+	c["oauth_consumer_key"] = creds_arr[0]
+	c["oauth_consumer_secret"] = creds_arr[1]
+	c["oauth_token"] = creds_arr[2]
+	c["oauth_token_secret"] = creds_arr[3]
 
-  c := make(map[string]string)
-
-  c["oauth_consumer_key"] = lines[0]
-  c["oauth_consumer_secret"] = lines[1]
-  c["oauth_token"] = lines[2]
-  c["oauth_token_secret"] = lines[3]
-
-  return c, nil
+	return c, nil
 }
 
 func (u *UStreamClient) Connect() (*http.Response, error) {
 
-  httpRequest, _ := http.NewRequest("GET", "https://userstream.twitter.com/1.1/user.json", nil)
-  u.service.Sign(httpRequest, u.user)
-  var httpResponse *http.Response
-  var err error
-  httpResponse, err = u.httpClient.Do(httpRequest)
+	httpRequest, _ := http.NewRequest("GET", "https://userstream.twitter.com/1.1/user.json", nil)
+	u.service.Sign(httpRequest, u.user)
+	var httpResponse *http.Response
+	var err error
+	httpResponse, err = u.httpClient.Do(httpRequest)
 
-  if err != nil {
-    log.Printf("Connection error")
-    return nil, err
-  }
+	if err != nil {
+		log.Printf("Connection error")
+		return nil, err
+	}
 
-  return httpResponse, nil
+	return httpResponse, nil
 }
 
-func (u *UStreamClient) ReadStream(resp *http.Response) (chan *Tweet) {
-  var reader *bufio.Reader
-  reader = bufio.NewReader(resp.Body)
+func (u *UStreamClient) ReadStream(resp *http.Response) chan *Tweet {
+	var reader *bufio.Reader
+	reader = bufio.NewReader(resp.Body)
 
-  go func() {
-  for {
+	go func() {
+		for {
 
-      line, err := reader.ReadBytes('\n')
+			line, err := reader.ReadBytes('\n')
 
-      if err != nil {
-        log.Printf("Error reading line of response\n")
-      }
+			if err != nil {
+				log.Printf("Error reading line of response\n")
+			}
 
-      line = bytes.TrimSpace(line)
+			line = bytes.TrimSpace(line)
 
-      if len(line) == 0 {
-          continue
-      }
+			if len(line) == 0 {
+				continue
+			}
 
-      // Skip over friendlist and events for now
-      if bytes.HasPrefix(line, []byte(`{"event":`)) {
-        continue
-      }
-      if bytes.HasPrefix(line, []byte(`{"friends":`)) {
-        continue
-      }
+			// Skip over friendlist and events for now
+			if bytes.HasPrefix(line, []byte(`{"event":`)) {
+				continue
+			}
+			if bytes.HasPrefix(line, []byte(`{"friends":`)) {
+				continue
+			}
 
-      // Unmarshal the tweet into a buffer
-      var buffer map[string]interface{}
-      json.Unmarshal(line, &buffer)
+			// Unmarshal the tweet into a buffer
+			var buffer map[string]interface{}
+			json.Unmarshal(line, &buffer)
 
-      var tweet *Tweet
+			var tweet *Tweet
 
-      // Only grab the information we want from the unmarshalled data
-      if buffer["id"] != 0 && buffer["text"] != "" {
+			// Only grab the information we want from the unmarshalled data
+			if buffer["id"] != 0 && buffer["text"] != "" {
 
-        id_str, ok := buffer["id_str"].(string)
+				id_str, ok := buffer["id_str"].(string)
 
-        if !ok {
-          log.Printf("Error converting Tweet ID: %#v", buffer["id_str"])
-          continue
-        }
+				if !ok {
+					log.Printf("Error converting Tweet ID: %#v", buffer["id_str"])
+					continue
+				}
 
-        text, ok := buffer["text"].(string)
+				text, ok := buffer["text"].(string)
 
-        if !ok {
-          log.Printf("Error converting Tweet text")
-          continue
-        }
+				if !ok {
+					log.Printf("Error converting Tweet text")
+					continue
+				}
 
-        in_reply_to_status_id_str, ok := buffer["in_reply_to_status_id_str"].(string)
+				in_reply_to_status_id_str, ok := buffer["in_reply_to_status_id_str"].(string)
 
-        if string(in_reply_to_status_id_str) == "null" {
-          log.Printf("Error converting Tweet reply status id str")
-        }
+				if string(in_reply_to_status_id_str) == "null" {
+					log.Printf("Error converting Tweet reply status id str")
+				}
 
-        if buffer["user"] != nil {
+				if buffer["user"] != nil {
 
-          user, ok := buffer["user"].(map[string]interface{})
+					user, ok := buffer["user"].(map[string]interface{})
 
-          if !ok {
-            log.Printf("Error converting Tweet User")
-            continue
-          }
+					if !ok {
+						log.Printf("Error converting Tweet User")
+						continue
+					}
 
-          name, ok := user["name"].(string)
+					name, ok := user["name"].(string)
 
-          if !ok {
-            log.Printf("Error converting Tweet User Name")
-            continue
-          }
+					if !ok {
+						log.Printf("Error converting Tweet User Name")
+						continue
+					}
 
-          screen_name, ok := user["screen_name"].(string)
+					screen_name, ok := user["screen_name"].(string)
 
-          if !ok {
-            log.Printf("Error converting Tweet User ScreenName")
-            continue
-          }
+					if !ok {
+						log.Printf("Error converting Tweet User ScreenName")
+						continue
+					}
 
-          // Create Tweet
-          tweet = &Tweet{ id_str, text, &User{ screen_name, name }, in_reply_to_status_id_str}
-        }
-      }
+					// Create Tweet
+					tweet = &Tweet{id_str, text, &User{screen_name, name}, in_reply_to_status_id_str}
+				}
+			}
 
-      u.stream <- tweet
-  }
-  }()
+			u.stream <- tweet
+		}
+	}()
 
-  return u.stream
+	return u.stream
 }
